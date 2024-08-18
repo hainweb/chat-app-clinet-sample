4319d6f4972c704056c2c77b24d14ad6950a72d0
hainweb <hainofficialweb@gmail.com>
2024-08-18 15:34:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
+import io from 'socket.io-client';
+
+const socket = io(BACKEND_URL, {
+  withCredentials: true,
+  transports: ['websocket', 'polling']
+});
 
 function App() {
   return (
